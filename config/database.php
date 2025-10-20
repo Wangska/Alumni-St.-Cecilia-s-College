075ea3882f4eb7108f2c2a92fc65ceb6a5dc1452
Wangska <wangska1283@gmail.com>
2025-10-20 20:11:40 +0800

    changes final

--- config/database.php
+++ config/database.php
@@ -2,10 +2,12 @@
 declare(strict_types=1);
 
 return [
-    'host' => '127.0.0.1',
-    'database' => 'sccalumni_db',
-    'username' => 'root',
-    'password' => '',
+    'host' => getenv('DB_HOST') ?: '127.0.0.1',
+    'database' => getenv('DB_DATABASE') ?: 'sccalumni_db',
+    'username' => getenv('DB_USERNAME') ?: 'root',
+    'password' => getenv('DB_PASSWORD') ?: '',
+    'port' => (int)(getenv('DB_PORT') ?: 3306),
+    'driver' => getenv('DB_CONNECTION') ?: 'mysql',
     'charset' => 'utf8mb4',
 ];
 
